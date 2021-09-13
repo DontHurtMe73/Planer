036855fbdc238d67a6b25f136cb240b366edf0d1
Vladislav Voronov <voronovvladislav73@gmail.com>
2021-09-13 14:16:46 +0300

    add local storage

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 let form = document.querySelector('#addForm');
 let itemsList = document.querySelector('#items');
+let inputForm = document.querySelector("#newItemText");
+let newItemText;
 let searchInput = document.querySelector('#filter');
 
 
+//local Storage
+let todos;
+function toLocal() {
+   todos = itemsList.innerHTML;
+   localStorage.setItem('test', todos)
+}
+
+if (localStorage.getItem('test')) {
+   itemsList.innerHTML = localStorage.getItem('test')
+}
+
+//Listeners
 form.addEventListener('submit', addItem);
 itemsList.addEventListener('click', remove);
 searchInput.addEventListener('keyup', search);
@@ -10,42 +24,42 @@ searchInput.addEventListener('keyup', search);
 //Invite task
 function addItem(e) {
    e.preventDefault();
-   let inputForm = document.querySelector("#newItemText");
-   let newItemText = inputForm.value;
-   let newTemplate = `<li class="list-group-item">
+   newItemText = inputForm.value;
+   if (newItemText === '') {
+      return
+   }
+   let newTemplate = `
+   <li class="list-group-item">
       ${newItemText}
       <button data-action="delete" type="button" class="btn btn-light btn-sm float-right">
          Удалить
       </button>
    </li>`;
    itemsList.insertAdjacentHTML("afterbegin", newTemplate);
-
+   toLocal()
    inputForm.value = "";
 }
 
 //Delete task
 function remove(e) {
    if (e.target.getAttribute('data-action') == 'delete') {
-      if(confirm('Точно удалить задачу?')){
+      if (confirm('Точно удалить задачу?')) {
          e.target.parentNode.remove()
+         toLocal()
       }
    }
 }
 
 //Search task
-function search(e){
+function search(e) {
    let searchedText = e.target.value.toLowerCase();
-
    let items = itemsList.querySelectorAll('li');
-   
-   items.forEach(function(item){
+   items.forEach(function (item) {
       let itemText = item.firstChild.textContent.toLowerCase();
-
       if (itemText.includes(searchedText)) {
-        item.style.display = "block";
+         item.style.display = "block";
       } else {
-        item.style.display = "none";
+         item.style.display = "none";
       }
-
    })
 }
